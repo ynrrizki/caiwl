cece89d371fd955bc34f9ba0b0846d7cd8d42eb9
ynrrizki <78051294+ynrrizki@users.noreply.github.com>
2023-08-20 13:56:07 +0700

    add new login admin ui

--- admin/login.php
+++ admin/login.php
@@ -1,179 +1,224 @@
 <?php
 require_once("../include/initialize.php");
 
- ?>
-  <?php
- // login confirmation
-  if(isset($_SESSION['USERID'])){
-    redirect(web_root."admin/index.php");
-  }
-  ?>
+?>
+<?php
+// login confirmation
+if (isset($_SESSION['USERID'])) {
+  redirect(web_root . "admin/index.php");
+}
+?>
 
- 
+
 
 <!-- Bootstrap core CSS -->
 
 
- 
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Login V18</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="<?php echo web_root; ?>css/bootstrap.min.css" rel="stylesheet"> 
- 
-<!--===============================================================================================-->  
-  <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?php echo web_root; ?>admin/adminMenu/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?php echo web_root; ?>admin/adminMenu/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?php echo web_root; ?>admin/adminMenu/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?php echo web_root; ?>admin/adminMenu/vendor/animate/animate.css">
-<!--===============================================================================================-->  
-  <link rel="stylesheet" type="text/css" href="<?php echo web_root; ?>admin/adminMenu/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?php echo web_root; ?>admin/adminMenu/vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?php echo web_root; ?>admin/adminMenu/vendor/select2/select2.min.css">
-<!--===============================================================================================-->  
-  <link rel="stylesheet" type="text/css" href="<?php echo web_root; ?>admin/adminMenu/vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?php echo web_root; ?>admin/adminMenu/css/util.css">
-  <link rel="stylesheet" type="text/css" href="<?php echo web_root; ?>admin/adminMenu/css/main.css">
-<!--===============================================================================================-->
+  <!-- <link href="<?php echo web_root; ?>css/bootstrap.min.css" rel="stylesheet"> -->
+  <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+
+  <!-- Icons. Uncomment required icon fonts -->
+  <link rel="stylesheet" href="<?php echo web_root; ?>sneat/assets/vendor/fonts/boxicons.css" />
+
+  <!-- Core CSS -->
+  <link rel="stylesheet" href="<?php echo web_root; ?>sneat/assets/vendor/css/core.css" class="template-customizer-core-css" />
+  <link rel="stylesheet" href="<?php echo web_root; ?>sneat/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+  <link rel="stylesheet" href="<?php echo web_root; ?>sneat/assets/css/demo.css" />
+
+  <!-- Vendors CSS -->
+  <link rel="stylesheet" href="<?php echo web_root; ?>sneat/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+  <!-- Page CSS -->
+  <!-- Page -->
+  <link rel="stylesheet" href="<?php echo web_root; ?>sneat/assets/vendor/css/pages/page-auth.css" />
+  <!-- Helpers -->
+  <script src="<?php echo web_root; ?>sneat/assets/vendor/js/helpers.js"></script>
+
+  <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+  <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+  <script src="<?php echo web_root; ?>sneat/assets/js/config.js"></script>
 </head>
-<style type="text/css">
-  .login100-more img {
-    width: 100%;
-    height: 100%;
-  }
-</style>
-<body style="background-color: #666666;">
-  
-  <div class="limiter">
-    <div class="container-login100">
-      <div class="wrap-login100">
-        <form class="login100-form validate-form" action="" method="POST">
-          <span class="login100-form-title p-b-43">
-            Login to continue
-          </span>
-          
-          <p><?php echo check_message()?>;</p>
-          <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-            <input class="input100" type="text" name="user_email">
-            <span class="focus-input100"></span>
-            <span class="label-input100">Username</span>
-          </div>
-          
-          
-          <div class="wrap-input100 validate-input" data-validate="Password is required">
-            <input class="input100" type="password" name="user_pass">
-            <span class="focus-input100"></span>
-            <span class="label-input100">Password</span>
-          </div>
 
-          <div class="flex-sb-m w-full p-t-3 p-b-32">
-           <!--  <div class="contact100-form-checkbox">
-              <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-              <label class="label-checkbox100" for="ckb1">
-                Remember me
-              </label>
-            </div> -->
+<body>
+  <!-- Content -->
 
-           <!--  <div>
-              <a href="#" class="txt1">
-                Forgot Password?
+  <div class="container-xxl">
+    <div class="authentication-wrapper authentication-basic container-p-y">
+      <div class="authentication-inner">
+        <!-- Register -->
+        <div class="card">
+          <div class="card-body">
+            <!-- Logo -->
+            <div class="app-brand justify-content-center">
+              <a href="index.html" class="app-brand-link gap-2">
+                <span class="app-brand-logo demo">
+                  <svg width="25" viewBox="0 0 25 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <defs>
+                      <path d="M13.7918663,0.358365126 L3.39788168,7.44174259 C0.566865006,9.69408886 -0.379795268,12.4788597 0.557900856,15.7960551 C0.68998853,16.2305145 1.09562888,17.7872135 3.12357076,19.2293357 C3.8146334,19.7207684 5.32369333,20.3834223 7.65075054,21.2172976 L7.59773219,21.2525164 L2.63468769,24.5493413 C0.445452254,26.3002124 0.0884951797,28.5083815 1.56381646,31.1738486 C2.83770406,32.8170431 5.20850219,33.2640127 7.09180128,32.5391577 C8.347334,32.0559211 11.4559176,30.0011079 16.4175519,26.3747182 C18.0338572,24.4997857 18.6973423,22.4544883 18.4080071,20.2388261 C17.963753,17.5346866 16.1776345,15.5799961 13.0496516,14.3747546 L10.9194936,13.4715819 L18.6192054,7.984237 L13.7918663,0.358365126 Z" id="path-1"></path>
+                      <path d="M5.47320593,6.00457225 C4.05321814,8.216144 4.36334763,10.0722806 6.40359441,11.5729822 C8.61520715,12.571656 10.0999176,13.2171421 10.8577257,13.5094407 L15.5088241,14.433041 L18.6192054,7.984237 C15.5364148,3.11535317 13.9273018,0.573395879 13.7918663,0.358365126 C13.5790555,0.511491653 10.8061687,2.3935607 5.47320593,6.00457225 Z" id="path-3"></path>
+                      <path d="M7.50063644,21.2294429 L12.3234468,23.3159332 C14.1688022,24.7579751 14.397098,26.4880487 13.008334,28.506154 C11.6195701,30.5242593 10.3099883,31.790241 9.07958868,32.3040991 C5.78142938,33.4346997 4.13234973,34 4.13234973,34 C4.13234973,34 2.75489982,33.0538207 2.37032616e-14,31.1614621 C-0.55822714,27.8186216 -0.55822714,26.0572515 -4.05231404e-15,25.8773518 C0.83734071,25.6075023 2.77988457,22.8248993 3.3049379,22.52991 C3.65497346,22.3332504 5.05353963,21.8997614 7.50063644,21.2294429 Z" id="path-4"></path>
+                      <path d="M20.6,7.13333333 L25.6,13.8 C26.2627417,14.6836556 26.0836556,15.9372583 25.2,16.6 C24.8538077,16.8596443 24.4327404,17 24,17 L14,17 C12.8954305,17 12,16.1045695 12,15 C12,14.5672596 12.1403557,14.1461923 12.4,13.8 L17.4,7.13333333 C18.0627417,6.24967773 19.3163444,6.07059163 20.2,6.73333333 C20.3516113,6.84704183 20.4862915,6.981722 20.6,7.13333333 Z" id="path-5"></path>
+                    </defs>
+                    <g id="g-app-brand" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                      <g id="Brand-Logo" transform="translate(-27.000000, -15.000000)">
+                        <g id="Icon" transform="translate(27.000000, 15.000000)">
+                          <g id="Mask" transform="translate(0.000000, 8.000000)">
+                            <mask id="mask-2" fill="white">
+                              <use xlink:href="#path-1"></use>
+                            </mask>
+                            <use fill="#696cff" xlink:href="#path-1"></use>
+                            <g id="Path-3" mask="url(#mask-2)">
+                              <use fill="#696cff" xlink:href="#path-3"></use>
+                              <use fill-opacity="0.2" fill="#FFFFFF" xlink:href="#path-3"></use>
+                            </g>
+                            <g id="Path-4" mask="url(#mask-2)">
+                              <use fill="#696cff" xlink:href="#path-4"></use>
+                              <use fill-opacity="0.2" fill="#FFFFFF" xlink:href="#path-4"></use>
+                            </g>
+                          </g>
+                          <g id="Triangle" transform="translate(19.000000, 11.000000) rotate(-300.000000) translate(-19.000000, -11.000000) ">
+                            <use fill="#696cff" xlink:href="#path-5"></use>
+                            <use fill-opacity="0.2" fill="#FFFFFF" xlink:href="#path-5"></use>
+                          </g>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </span>
+                <span class="app-brand-text demo text-body fw-bolder">Sneat</span>
               </a>
-            </div> -->
-          </div>
-      
+            </div>
+            <!-- /Logo -->
+            <h4 class="mb-2">Welcome to Sneat! 👋</h4>
+            <p class="mb-4">Please sign-in to your account and start the adventure</p>
 
-          <div class="container-login100-form-btn">
-            <button class="login100-form-btn" type="submit" name="btnLogin">
-              Login
-            </button>
-          </div>
-          
-          <div class="text-center p-t-46 p-b-20">
-            <span class="txt2">
-              or <a href="../index.php"><i class="fa fa-arrow-left"></i> Back to Home</a>
-            </span> 
-          </div>
-<!-- 
-          <div class="login100-form-social flex-c-m">
-            <a href="#" class="login100-form-social-item flex-c-m bg1 m-r-5">
-              <i class="fa fa-facebook-f" aria-hidden="true"></i>
-            </a>
+            <form id="formAuthentication" class="mb-3" method="POST">
+              <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username" name="user_email" placeholder="Enter your username" autofocus />
+              </div>
+              <div class="mb-3 form-password-toggle">
+                <div class="d-flex justify-content-between">
+                  <label class="form-label" for="password">Password</label>
+                </div>
+                <div class="input-group input-group-merge">
+                  <input type="password" id="password" class="form-control" name="user_pass" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
+                  <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                </div>
+              </div>
+              <div class="mb-3">
+                <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+              </div>
+            </form>
 
-            <a href="#" class="login100-form-social-item flex-c-m bg2 m-r-5">
-              <i class="fa fa-twitter" aria-hidden="true"></i>
-            </a>
-          </div> -->
-        </form>
-
-        <div class="login100-more" style="background: #0037a4f5;">
-          <img src="adminMenu/images/login.png" style="object-fit: contain">
+            <p class="text-center">
+              <a href="../index.php">
+                <i class="fa fa-arrow-left"></i>
+                <span>Back to Home</span>
+              </a>
+            </p>
+          </div>
         </div>
+        <!-- /Register -->
       </div>
     </div>
   </div>
-  
-  
 
-  
-  
-<!--===============================================================================================-->
-  <script src="<?php echo web_root; ?>admin/adminMenu/vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-  <script src="<?php echo web_root; ?>admin/adminMenu/vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-  <script src="<?php echo web_root; ?>admin/adminMenu/vendor/bootstrap/js/popper.js"></script>
-  <script src="<?php echo web_root; ?>admin/adminMenu/vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-  <script src="<?php echo web_root; ?>admin/adminMenu/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-  <script src="<?php echo web_root; ?>admin/adminMenu/vendor/daterangepicker/moment.min.js"></script>
-  <script src="<?php echo web_root; ?>admin/adminMenu/vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-  <script src="<?php echo web_root; ?>admin/adminMenu/vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
-  <script src="<?php echo web_root; ?>admin/adminMenu/js/main.js"></script>
+  <!-- / Content -->
+  <!-- <form class="login100-form validate-form" action="" method="POST">
+    <span class="login100-form-title p-b-43">
+      Login to continue
+    </span>
 
+    <p><?php // echo check_message() 
+        ?></p>
+    <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+      <input class="input100" type="text" name="user_email">
+      <span class="focus-input100"></span>
+      <span class="label-input100">Username</span>
+    </div>
+
+
+    <div class="wrap-input100 validate-input" data-validate="Password is required">
+      <input class="input100" type="password" name="user_pass">
+      <span class="focus-input100"></span>
+      <span class="label-input100">Password</span>
+    </div>
+
+
+    <div class="container-login100-form-btn">
+      <button class="login100-form-btn" type="submit" name="btnLogin">
+        Login
+      </button>
+    </div>
+
+    <div class="text-center p-t-46 p-b-20">
+      <span class="txt2">
+        <a href="../index.php">
+          <i class="fa fa-arrow-left"></i>
+          Back to Home
+        </a>
+      </span>
+    </div>
+  </form> -->
+
+  <!-- Core JS -->
+  <!-- build:js assets/vendor/js/core.js -->
+  <script src="<?php echo web_root; ?>sneat/assets/vendor/libs/jquery/jquery.js"></script>
+  <script src="<?php echo web_root; ?>sneat/assets/vendor/libs/popper/popper.js"></script>
+  <script src="<?php echo web_root; ?>sneat/assets/vendor/js/bootstrap.js"></script>
+  <script src="<?php echo web_root; ?>sneat/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+  <script src="<?php echo web_root; ?>sneat/assets/vendor/js/menu.js"></script>
+  <!-- endbuild -->
+
+  <!-- Vendors JS -->
+
+  <!-- Main JS -->
+  <script src="<?php echo web_root; ?>sneat/assets/js/main.js"></script>
 </body>
+
 </html>
 
 <?php
 
-if(isset($_POST['btnLogin'])){
+if (isset($_POST['btnLogin'])) {
   $email = trim($_POST['user_email']);
   $upass  = trim($_POST['user_pass']);
   $h_upass = sha1($upass);
 
-   if ($email == '' OR $upass == '') {
+  if ($email == '' or $upass == '') {
 
-      message("Invalid Username and Password!", "error");
-      redirect("login.php");
-
-    } else {
-  //it creates a new objects of member
+    message("Invalid Username and Password!", "error");
+    redirect("login.php");
+  } else {
+    //it creates a new objects of member
     $user = new User();
     //make use of the static function, and we passed to parameters
     $res = $user::userAuthentication($email, $h_upass);
-    if ($res==true) {
-       message("You login as ".$_SESSION['TYPE'].".","success");
-      if ($_SESSION['TYPE']=='Administrator'){
-         redirect(web_root."admin/index.php");
-      }else{
-           redirect(web_root."admin/login.php");
+    if ($res == true) {
+      message("You login as " . $_SESSION['TYPE'] . ".", "success");
+      if ($_SESSION['TYPE'] == 'Administrator') {
+        redirect(web_root . "admin/index.php");
+      } else {
+        redirect(web_root . "admin/login.php");
       }
-    }else{
+    } else {
       message("Account does not exist! Please contact Administrator.", "error");
-       redirect(web_root."admin/login.php");
+      redirect(web_root . "admin/login.php");
     }
- }
- }
- ?> 
+  }
+}
+?>
